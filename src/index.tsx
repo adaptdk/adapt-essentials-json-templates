172fd9a17de05f29ae42dc39459c8ef289495523
Toxicom <tomas.98.leo@gmail.com>
2023-12-15 13:36:22 +0200

    env

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import LocalhostWarning from "./components/LocalhostWarning";
 
-const container = document.getElementById(`root`);
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = document.getElementById(`root`)!;
 
-if (container) {
-  const root = createRoot(container);
+const root = createRoot(container);
 
-  if (process.env.NODE_ENV === `development` && window.self === window.top) {
-    // You can remove this if block before deploying your app
-    root.render(<LocalhostWarning />);
-  } else {
-    root.render(
-      <SDKProvider>
-        <GlobalStyles />
-        <App />
-      </SDKProvider>,
-    );
-  }
+if (process.env.NODE_ENV === `development` && window.self === window.top) {
+  // You can remove this if block before deploying your app
+  root.render(<LocalhostWarning />);
+} else {
+  root.render(
+    <SDKProvider>
+      <GlobalStyles />
+      <App />
+    </SDKProvider>,
+  );
 }
